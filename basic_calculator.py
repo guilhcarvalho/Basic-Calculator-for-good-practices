9eff2e3ff7a63d595a035fb545040c1347e4a6d8
Guilherme Carvalho <143360790+guilhcarvalho@users.noreply.github.com>
2024-12-19 20:52:34 -0300

    Create basic_calculator.py

---
+++ basic_calculator.py
@@ -0,0 +1,43 @@
+'''
+My goal here is to build a basic calculator for my learning
+'''
+
+#Calculator operations functions:
+def plus(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+#Building an input operation
+operation = input(f'Select an operator: "+"  "-"  "*" "/" -->   ')
+
+# X and Y values input:
+x = float(input('Input a value:  '))
+y = float(input('Input a value:  '))
+
+#Conditions for operations:
+if operation == '+':
+    result = plus(x, y)
+
+elif operation == '-':
+    result = sub(x, y)
+
+elif operation == '*':
+    result = multiply(x, y)
+    
+elif operation == '/':
+    if y != 0:
+        result = divide(x, y)
+    else:
+        print(f'ERROR, Division by 0')
+        exit()
+        
+#Print of result
+print(f'Result of this operation is {result}.')
